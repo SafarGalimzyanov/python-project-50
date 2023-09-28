52747a671e75ea4736f39ef2e522923139382708
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-09-28 21:33:50 +0600

    Step 4: add gendiff_script ; add gendiff_script (pyproject.toml)

---
+++ gendiff/scripts/gendiff_script.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import gendiff.gendiff_info as g_i
+from typing import TextIO
+
+
+def main():
+   parser = argparse.ArgumentParser(description = g_i.DESCRIPTION)
+   parser.add_argument('text_file', metavar=g_i.POS_ARGS['first_arg'], type=TextIO)
+   parser.add_argument('text_file', metavar=g_i.POS_ARGS['second_arg'], type=TextIO)
+
+   args = parser.parse_args()
+   print(args.dest(args.text_file))
+
+
+if __name__ == '__main__':
+    main()
