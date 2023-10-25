46e4e3f3f9a34aeee81549e0a11d697c4c64cf68
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-10-25 12:09:37 +0600

    Step 8: update tests/ ; update .gitignore

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -12,35 +12,46 @@ def get_json_text(file_path: str) -> dict:
 def _json_regular() -> str:
     return 'tests/fixtures/json_regular.json'
 
-@pytest.fixture(name='json_same_values')
+@pytest.fixture(name='json_same_val')
 def _json_regular() -> str:
-    return 'tests/fixtures/json_same_values.json'
+    return 'tests/fixtures/json_same_val.json'
 
-@pytest.fixture(name='json_unique_values')
+@pytest.fixture(name='json_unique_val')
 def _json_regular() -> str:
-    return 'tests/fixtures/json_unique_values.json'
-@pytest.fixture(name='json_unique_pairs')
+    return 'tests/fixtures/json_unique_val.json'
+
+@pytest.fixture(name='json_unique_pair')
 def _json_unique_pairs() -> str:
-    return 'tests/fixtures/json_unique_pairs.json'
+    return 'tests/fixtures/json_unique_pair.json'
+
 @pytest.fixture(name='json_empty')
 def _json_regular() -> str:
     return 'tests/fixtures/json_empty.json'
 
 
 def test_generate_diff_json():
-    reg_and_same_value =
+    reg_and_same_val =
     '{\n\n    1k: 1v\n    2k: 1v\n  - 3k: 3v\n  + 3k: 1v\n\n}'
-    reg_and_unique =
-    '{\n\n    1k: 1v\n  - 2K: 1v\n  + 2k: 2v\n    3k: 3v\n}'
+    reg_and_unique_val =
+    '{\n\n    1k: 1v\n  - 2K: 1v\n  + 2k: 2v\n    3k: 3v\n\n}'
+    reg_and_unique_pair =
+    '{\n\n  + 0k: 0v\n  - 1k: 1v\n  - 2k: 1v\n  + 4k: 1v\n  - 3k: 3v\n  + true: []\n\n}'
     reg_and_empty =
     '{\n\n  - 1k: 1v\n  - 2k: 1v\n  - 3k: 3v\n\n}'
-    same_values_and_unique_values =
-    '{\n\n    1k: 1v\n  - 2k: 1v\n  + 2k: 2v\n  - 3k: 1v\n  + 3k: 3v\n}'
-    same_values_and_empty =
-    '{\n\n  - 1k: 1v\n  - 2k: 1v\n  - 3k: 1v\n}'
-    unique_values_and_empty =
-    '{\n\n  - 1k: 1v\n  - 2k: 2v\n  - 3k: 3v\n}'
-    assert g_d('json_regular', 'json_empty') == reg_and_same_value
+    same_val_and_unique_val =
+    '{\n\n    1k: 1v\n  - 2k: 1v\n  + 2k: 2v\n  - 3k: 1v\n  + 3k: 3v\n\n}'
+    same_val_and_empty =
+    '{\n\n  - 1k: 1v\n  - 2k: 1v\n  - 3k: 1v\n\n}'
+    unique_val_and_empty =
+    '{\n\n  - 1k: 1v\n  - 2k: 2v\n  - 3k: 3v\n\n}'
+
+    assert g_d('json_regular', 'json_same_val') == reg_and_same_val
+    assert g_d('json_regular', 'json_unique_val') == reg_and_unique_val
+    assert g_d('json_regular', 'json_unique') == reg_and_unique_pair
+    assert g_d('json_regular', 'json_empty') == reg_and_empty
+    assert g_d('json_same_val', 'json_unique_val') == same_val_and_unique_val
+    assert g_d('json_same_val', 'json_empty') == same_val_and_empty
+    assert g_d('json_unique_val', 'json_empty') == unique_val_and_empty
 
 
 
