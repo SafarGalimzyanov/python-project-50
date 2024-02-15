17a41ec44783af80c5a6f8692210122b962756a4
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2024-02-16 02:04:28 +0600

    14.3: update stylize gendiff_engine

--- gendiff/gendiff_engine.py
+++ gendiff/gendiff_engine.py
@@ -2,75 +2,53 @@ from copy import deepcopy
 from stylish.stylize import added, removed, same, updated
 
 
-def compare_dicts(d: dict={}, depth: int=0) -> str:
-        result = ': {\n'
-        indent = depth*' '
-        for key in d:
-            v = d.get(key)
-            key_v = f'{key}: {v}'
-            if type(v) is dict:
-                result += f'{indent}{key}{print_dict(v, depth+4)}\n'
-            else:
-                result += f'{indent}{key_v}\n'
-        result += indent[4:] + '}'
-        return result
-
-
 def compare_files(d1, d2, style: str, depth: int=4, space: int=2) -> str:
     result = ''
-    keys = sorted(tuple(set(d1.keys()).union(set(d2.keys()))))
-    
+    keys = sorted(tuple(set(d1.keys()).union(set(d2.keys()))))    
     indent = space*' '
+    '''
     default = indent + '  '
     key_in_dict1 = indent + '- '
     key_in_dict2 = indent + '+ '
     dict_open = ': {'
     dict_close = indent + ' }'
-
+    '''
     for key in keys:
         v1 = d1.get(key)
         key_v1 = f'{key}: {v1}'
         v2 = d2.get(key)
         key_v2 = f'{key}: {v2}'
         
-
         if key in d1 and key in d2: #same keys
             if type(v1) is dict and type(v2) is dict:
                 if v1 != v2: #special
                     result += f'{indent}  {key}: "{"')
-                    compare_files(v1, v2, depth+4, space+4)
+                    compare_files(v1, v2, style, depth+4, space+4)
                     result += f'{indent} "}"'
                 else:
-                    result += same(indent, depth, key, v1, print_format)
-                    result = 
-                    result = f'{default}{key}{print_dict(v1, depth+4)}' 
+                    result += same(key, v1, style, depth, indent)
+                    #result = f'{default}{key}{print_dict(v1, depth+4)}' 
             else:
-                if v1 != v2: #diff values -> show both
-                    result += updated(indent, depth, key, v1, v2, print_format)
-                    result = 
-                    result = f'{key_in_dict1}{key_v1}\n{key_in_dict2}{key_v2}'
-                else: #same values -> show any
-                    result += same(indent, depth, key, v1, print_format)
-                    result = 
-                    result = f'{default}{key_v1}'
+                if v1 != v2:
+                    result += updated(key, v1, v2, style, depth, indent)
+                    #result = f'{key_in_dict1}{key_v1}\n{key_in_dict2}{key_v2}'
+                else:
+                    result += same(key, v1, style, depth, indent)
+                    #result = f'{default}{key_v1}'
         elif key in d1 and key not in d2: #key in dict 1
-            if type(v1) is dict: #dict -> print_dict()
-                result += removed(indent, depth, key, v1, print_format) #определяем, что словарь внутри функции
-                result = 
-                result = f'{key_in_dict1}{key}{print_dict(v1, depth+4)}'
+            if type(v1) is dict:
+                result += removed(key, v1, style, depth, indent)
+                #result = f'{key_in_dict1}{key}{print_dict(v1, depth+4)}'
             else:
-                result += removed(indent, depth, key, v1, print_format)
-                result = 
-                result = f'{key_in_dict1}{key_v1}'
-        else: #key in dict 2
+                result += removed(key, v1, style, depth, indent)
+                #result = f'{key_in_dict1}{key_v1}'
+        else:
             if type(v2) is dict: #dict -> print_dict()
-                result += added(indent, depth, key, v2, print_format)
-                result = 
-                result = f'{key_in_dict2}{key}{print_dict(v2, depth+4)}'
+                result += added(key, v2, style, depth, indent)
+                #result = f'{key_in_dict2}{key}{print_dict(v2, depth+4)}'
             else:
-                result += added(indent, depth, key, v2, print_format)
-                result = 
-                result = f'{key_in_dict2}{key_v2}'
+                result += added(key, v2, style, depth, indent)
+                #result = f'{key_in_dict2}{key_v2}'
 
     return result
 
--- stylish/stylize.py
+++ stylish/stylize.py
@@ -1,99 +1,85 @@
-def added(indent, depth, key, value_2, style: str='') -> str:
-    if style == '--plain':
-        if type(value_2) is dict:
-            return '{key_in_dict2}{key}{print_dict(v2, depth+4)}'
+def dict_to_str(d, depth) -> str:
+    result = ': {\n'
+    indent = depth*' '
+    for key in d:
+        v = d.get(key)
+        key_v = f'{key}: {v}'
+        if type(v) is dict:
+            result += f'{indent}{key}{dict_to_str(v, depth+4)}\n'
         else:
-            return '{key_in_dict2}{key_v2}'
-    else:
-        if type(value_2) is dict:
-            return '{key_in_dict2}{key}{print_dict(v2, depth+4)}'
-        else:
-            return '{key_in_dict2}{key_v2}'
-
-
-
-
-def removed(indent, key, value_1, value_2=0) -> str:
-    if style == '--plain':
-        if type(value_2) is dict:
-            return '{key_in_dict1}{key}{print_dict(value_1, depth+4)}'
-        else:
-            return '{key_in_dict1}{key_v1}'
-    else:
-        if type(value_2) is dict:
-            return '{key_in_dict1}{key}{print_dict(value_1, depth+4)}'
-        else:
-            return '{key_in_dict1}{key_v1}'
+            result += f'{indent}{key_v}\n'
+    result += indent[4:] + '}'
     return result
 
-                result = f'{key_in_dict1}{key}{print_dict(v1, depth+4)}'
-                result = f'{key_in_dict1}{key_v1}'
 
+def added(key, v2, style, depth, indent) -> str:
+    key_in_dict2 = indent + '+ '
+    key_v2 = f'{key}: {v2}'
 
-
-def updated(indent, key, value_1, value_2=0) -> str:
     if style == '--plain':
         if type(value_2) is dict:
-            return '{key_in_dict2}{key}{print_dict(v2, depth+4)}'
+            return '{key_in_dict2}{key}{dict_to_str(v2, depth+4)}'
         else:
             return '{key_in_dict2}{key_v2}'
     else:
         if type(value_2) is dict:
-            return '{key_in_dict2}{key}{print_dict(v2, depth+4)}'
+            return '{key_in_dict2}{key}{dict_to_str(v2, depth+4)}'
         else:
             return '{key_in_dict2}{key_v2}'
-    return result
-#not dicts
-                    result += updated(indent, key, v1, v2, print_format)
-                    result = f'{key_in_dict1}{key_v1}\n{key_in_dict2}{key_v2}'
-print(f'{default}{key}{dict_open}')
-                    print_diff(v1, v2, depth+4, space+4) #print result here
-                    print(f'{dict_close}', end='')
-                    result = ''  #finished -> next key
- 
 
 
 
-def same_value(indent, key, v1) -> str:
-#dicts
-                    result = f'{default}{key}{print_dict(v1, depth+4)}' 
-                    result = f'{default}{key_v1}'
+
+def removed(key, v1, style, depth, indent) -> str:
+    key_in_dict1 = indent + '- '
+    key_v1 = f'{key}: {v1}'
+
+    if style == '--plain':
+        if type(value_2) is dict:
+            return f'{key_in_dict1}{key}{dict_to_str(value_1, depth+4)}'
+        else:
+            return f'{key_in_dict1}{key_v1}'
+    else:
+        if type(value_2) is dict:
+            return f'{key_in_dict1}{key}{dict_to_str(value_1, depth+4)}'
+        else:
+            return f'{key_in_dict1}{key_v1}'
+
+
+def updated(key, v1, v2, style, depth, indent) -> str:
+    key_in_dict1 = indent + '- '
+    key_in_dict2 = indent + '+ '
+    key_v1 = f'{key}: {v1}'
+    key_v2 = f'{key}: {v2}'
+
+    if style == '--plain':
+        return f'{key_in_dict1}{key_v1}\n{key_in_dict2}{key_v2}'
+    else:
+        return f'{key_in_dict1}{key_v1}\n{key_in_dict2}{key_v2}'
+
+
+def same(key, v1, style, depth, indent) -> str:
+    default = indent + '  '
+    key_v1 = f'{key}: {v1}'
+
+    if style == '--plain':
+        if type(value_2) is dict:
+            return f'{default}{key}{dict_to_str(v1, depth+4)}'
+        else:
+            return f'{default}{key_v1}'
+    else:
+        if type(value_2) is dict:
+            return f'{default}{key}{dict_to_str(v2, depth+4)}'
+        else:
+            return f'{default}{key_v1}'
 
 
 def stylize(format_flag: str='') -> dict:
-
-
+???
+    '''
+    #INDENT
     indent = space*' '
     default = indent + '  '
     key_in_dict1 = indent + '- '
     key_in_dict2 = indent + '+ '
-    dict_open = ': {'
-    dict_close = indent + ' }'
-
-
-
-
-    def print_dict_plain(d: dict={}, depth: int=0) -> str:
-        result = ': {\n'
-        indent = depth*' '
-        for key in d:
-            v = d.get(key)
-            key_v = f'{key}: {v}'
-            if type(v) is dict:
-                result += f'{indent}{key}{print_dict(v, depth+4)}\n'
-            else:
-                result += f'{indent}{key_v}\n'
-        result += indent[4:] + '}'
-        return result
-    def print_dict_default(d: dict={}, depth: int=0) -> str:
-        result = ': {\n'
-        indent = depth*' '
-        for key in d:
-            v = d.get(key)
-            key_v = f'{key}: {v}'
-            if type(v) is dict:
-                result += f'{indent}{key}{print_dict(v, depth+4)}\n'
-            else:
-                result += f'{indent}{key_v}\n'
-        result += indent[4:] + '}'
-        return result
+    '''
