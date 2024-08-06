c58609a0df644705a17be5dc6800994c2db33cdb
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2024-08-06 20:35:17 +0500

    17.0: add files for more tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -12,8 +12,29 @@ DIFF_PLAIN_FORMAT = get_text('tests/fixtures/test_plain_format')
 DIFF_JSON_FORMAT = get_text('tests/fixtures/test_json_format.json')
 JSON_1 = 'tests/fixtures/json/file1.json'
 JSON_2 = 'tests/fixtures/json/file2.json'
+JSON_3 = 'tests/fixtures/json/file3.json'
+JSON_4 = 'tests/fixtures/json/file4.json'
+JSON_5 = 'tests/fixtures/json/file5.json'
+JSON_6 = 'tests/fixtures/json/file6.json'
+JSON_7 = 'tests/fixtures/json/file7.json'
+JSON_8 = 'tests/fixtures/json/file8.json'
+JSON_9 = 'tests/fixtures/json/file9.json'
+JSON_10 = 'tests/fixtures/json/file10.json'
+JSON_11 = 'tests/fixtures/json/file11.json'
+JSON_12 = 'tests/fixtures/json/file12.json'
+
 YAML_1 = 'tests/fixtures/yaml/file1.yaml'
-YAML_2 = 'tests/fixtures/yaml/file2.yml'
+YAML_2 = 'tests/fixtures/yaml/file2.yaml'
+YAML_3 = 'tests/fixtures/yaml/file3.yml'
+YAML_4 = 'tests/fixtures/yaml/file4.yaml'
+YAML_5 = 'tests/fixtures/yaml/file5.yml'
+YAML_6 = 'tests/fixtures/yaml/file6.yaml'
+YAML_7 = 'tests/fixtures/yaml/file7.yml'
+YAML_8 = 'tests/fixtures/yaml/file8.yaml'
+YAML_9 = 'tests/fixtures/yaml/file9.yml'
+YAML_10 = 'tests/fixtures/yaml/file10.yaml'
+YAML_11 = 'tests/fixtures/yaml/file11.yml'
+YAML_12 = 'tests/fixtures/yaml/file12.yml'
 
 
 def test_gendiff_json_default_style():
